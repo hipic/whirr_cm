8e31c945a9d723034aefc52a2897cb3590ac28f5
Jongwook Woo <dalgual@Linux-26-64bit>
2013-06-28 17:56:08 +0900

    add setup, start, stop shells

---
+++ startWhirrCM.sh
@@ -0,0 +1,1 @@
+whirr launch-cluster --config cm-ec2-hipic.properties --private-key-file ~/.ssh/id_rsa_whirr
---
+++ stopWhirrCM.sh
@@ -0,0 +1,1 @@
+whirr stop-cluster --config cm-ec2-hipic.properties --private-key-file ~/.ssh/id_rsa_whirr
